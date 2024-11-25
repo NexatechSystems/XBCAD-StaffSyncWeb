a61a42e9b8ba5aef6b91fd0e508d3d64ace5f832
mohammed hoque <ST10028317@vcconnect.edu.za>
2024-11-25 17:43:03 +0200

    final submission

--- obj/Debug/net8.0/StaffSyncWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/StaffSyncWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StaffSyncWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f34a4179fb99a5cc618df2763f125106dfb98b19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StaffSyncWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StaffSyncWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
